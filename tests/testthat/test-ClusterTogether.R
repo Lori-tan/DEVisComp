04ef295e2fc57496847b9fae4bf6311e3075ba35
Luomeng Tan <luomeng.tan@mail.utoronto.ca>
2021-12-06 16:38:00 -0500

    Changed variable names to camelStyle in tests.

--- tests/testthat/test-ClusterTogether.R
+++ tests/testthat/test-ClusterTogether.R
@@ -11,49 +11,49 @@ test_that("invalid inputs", {
                                         tidy=TRUE)))
   dds <- DESeq2::estimateSizeFactors(dds)
 
-  deseq2_normalized_counts <- DESeq2::counts(dds, normalized=TRUE)
+  deseq2NormalizedCounts <- DESeq2::counts(dds, normalized=TRUE)
 
   dds@colData$dex <- relevel(dds@colData$dex, ref = "control")
   dds <- DESeq2::DESeq(dds, quiet = TRUE)
   res <- DESeq2::results(dds, tidy=TRUE)
-  deseq2_result <- DESeq2::lfcShrink(dds, coef = "dex_treated_vs_control",
+  deseq2Result <- DESeq2::lfcShrink(dds, coef = "dex_treated_vs_control",
                                      type = "apeglm", quiet = TRUE)
 
   # get edgeR results
   counts <- data.frame(airwayCounts[,-1], row.names = airwayCounts$ensgene)
-  diff_list <- edgeR::DGEList(counts, samples = airwayMetadata)
-  edger_normalized_counts <- edgeR::cpm(diff_list, normalized.lib.sizes = TRUE)
+  diffList <- edgeR::DGEList(counts, samples = airwayMetadata)
+  edgerNormalizedCounts <- edgeR::cpm(diffList, normalized.lib.sizes = TRUE)
   dex <- factor(rep(c("control", "treated"), 4))
   design <- model.matrix(~dex)
-  rownames(design) <- colnames(diff_list)
-  diff_list <- edgeR::estimateDisp(diff_list, design, robust = TRUE)
-  fit <- edgeR::glmFit(diff_list, design)
+  rownames(design) <- colnames(diffList)
+  diffList <- edgeR::estimateDisp(diffList, design, robust = TRUE)
+  fit <- edgeR::glmFit(diffList, design)
   lrt <- edgeR::glmLRT(fit)
-  edger_result <- edgeR::topTags(lrt, n = dim(lrt)[1])$table
+  edgerResult <- edgeR::topTags(lrt, n = dim(lrt)[1])$table
 
   # Test
-  expect_error(ClusterTogether(1, deseq2_result, "DESeq2"),
-               "Normalized_counts should be a matirx or data frame.")
+  expect_error(ClusterTogether(1, deseq2Result, "DESeq2"),
+               "normalizedCounts should be a matirx or data frame.")
 
   # mock one invalid matrix
-  fail_counts <- deseq2_normalized_counts
-  fail_counts[1, 1] <- "a"
-  expect_error(ClusterTogether(fail_counts, deseq2_result, "DESeq2"),
-               "Normalized_counts should be a numeric")
+  failCounts <- deseq2NormalizedCounts
+  failCounts[1, 1] <- "a"
+  expect_error(ClusterTogether(failCounts, deseq2Result, "DESeq2"),
+               "normalizedCounts should be a numeric")
 
   # mock one counts that has different genes than the DE result
-  fail_counts <- deseq2_normalized_counts[-1, ]
-  expect_error(ClusterTogether(fail_counts, deseq2_result, "DESeq2"),
-               "Rownames of normalized_counts")
+  failCounts <- deseq2NormalizedCounts[-1, ]
+  expect_error(ClusterTogether(failCounts, deseq2Result, "DESeq2"),
+               "Rownames of normalizedCounts")
 
-  expect_error(ClusterTogether(deseq2_normalized_counts, deseq2_result, "deseq2"),
+  expect_error(ClusterTogether(deseq2NormalizedCounts, deseq2Result, "deseq2"),
                "Tool should be either DESeq2 or edgeR.")
 
-  expect_error(ClusterTogether(deseq2_normalized_counts, deseq2_result, "edgeR"),
-               "DE_result should be a data frame")
+  expect_error(ClusterTogether(deseq2NormalizedCounts, deseq2Result, "edgeR"),
+               "DEResult should be a data frame")
 
-  expect_error(ClusterTogether(edger_normalized_counts, edger_result, "DESeq2"),
-               "DE_result should be a DESeqResults object.")
+  expect_error(ClusterTogether(edgerNormalizedCounts, edgerResult, "DESeq2"),
+               "DEResult should be a DESeqResults object.")
 })
 
 # [END]
--- tests/testthat/test-compMA.R
+++ tests/testthat/test-compMA.R
@@ -11,44 +11,44 @@ test_that("invalid inputs", {
                                             tidy=TRUE)))
   dds <- DESeq2::estimateSizeFactors(dds)
 
-  deseq2_normalized_counts <- DESeq2::counts(dds, normalized=TRUE)
+  deseq2NormalizedCounts <- DESeq2::counts(dds, normalized=TRUE)
 
   dds@colData$dex <- relevel(dds@colData$dex, ref = "control")
   dds <- DESeq2::DESeq(dds, quiet = TRUE)
   res <- DESeq2::results(dds, tidy=TRUE)
-  deseq2_result <- DESeq2::lfcShrink(dds, coef = "dex_treated_vs_control",
+  deseq2Result <- DESeq2::lfcShrink(dds, coef = "dex_treated_vs_control",
                                      type = "apeglm", quiet = TRUE)
 
   # get edgeR results
   counts <- data.frame(airwayCounts[,-1], row.names = airwayCounts$ensgene)
-  diff_list <- edgeR::DGEList(counts, samples = airwayMetadata)
-  edger_normalized_counts <- edgeR::cpm(diff_list, normalized.lib.sizes = TRUE)
+  diffList <- edgeR::DGEList(counts, samples = airwayMetadata)
+  edgerNormalizedCounts <- edgeR::cpm(diffList, normalized.lib.sizes = TRUE)
   dex <- factor(rep(c("control", "treated"), 4))
   design <- model.matrix(~dex)
-  rownames(design) <- colnames(diff_list)
-  diff_list <- edgeR::estimateDisp(diff_list, design, robust = TRUE)
-  fit <- edgeR::glmFit(diff_list, design)
+  rownames(design) <- colnames(diffList)
+  diffList <- edgeR::estimateDisp(diffList, design, robust = TRUE)
+  fit <- edgeR::glmFit(diffList, design)
   lrt <- edgeR::glmLRT(fit)
-  edger_result <- edgeR::topTags(lrt, n = dim(lrt)[1])$table
+  edgerResult <- edgeR::topTags(lrt, n = dim(lrt)[1])$table
 
   # Test
-  expect_error(compMA(deseq2_result, edger_result[-1, ]),
-               "deseq2_result should contain same genes as edger_result.")
+  expect_error(compMA(deseq2Result, edgerResult[-1, ]),
+               "deseq2Result should contain same genes as edgerResult.")
 
-  expect_error(compMA(deseq2_result, edger_result, cutoff = "a"),
+  expect_error(compMA(deseq2Result, edgerResult, cutoff = "a"),
                "Cutoff should be a positive number between 0 and 1.")
 
-  expect_error(compMA(deseq2_result, edger_result, cutoff = -1),
+  expect_error(compMA(deseq2Result, edgerResult, cutoff = -1),
                "Cutoff should be a positive number between 0 and 1.")
 
-  expect_error(compMA(deseq2_result, edger_result, cutoff = 2),
+  expect_error(compMA(deseq2Result, edgerResult, cutoff = 2),
                "Cutoff should be a positive number between 0 and 1.")
 
-  expect_error(compMA(edger_result, edger_result),
-               "deseq2_result should be a DESeqResults object.")
+  expect_error(compMA(edgerResult, edgerResult),
+               "deseq2Result should be a DESeqResults object.")
 
-  expect_error(compMA(deseq2_result, deseq2_result),
-               "edger_result should be a data frame")
+  expect_error(compMA(deseq2Result, deseq2Result),
+               "edgerResult should be a data frame")
 })
 
 # [END]
